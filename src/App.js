bd8a4701999fc117f2968d647c807053f3d9edac
Leandro Lopez <leandrolopez131982@gmail.com>
2022-11-21 10:44:29 -0300

    curso bluu

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Contador from './component/Contador';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo React</h1>
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Contador.js
@@ -0,0 +1,20 @@
+import React, {useState, Fragment} from 'react';
+
+const Contador = () => {
+
+    const [numero, setNumero] = useState(0);
+
+    const aumentar = () => {
+        console.log("clik")
+        setNumero(numero + 1)
+    }
+    
+    return (
+        <Fragment>
+            <h3>Mi primer component {numero}</h3>
+            <button onClick={aumentar}>aumentar</button>
+        </Fragment>
+    );
+}
+
+export default Contador;
